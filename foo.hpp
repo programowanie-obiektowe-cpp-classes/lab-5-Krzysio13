ff31d361b12a5741be70b78352276938891070db
Krzysio13 <01171657@pw.edu.pl>
2024-12-02 12:00:41 +0100

    v1 (to akurat to samo co w zad6 XD)

--- foo.hpp
+++ foo.hpp
@@ -1,12 +1,23 @@
 #pragma once
 
 #include "Human.hpp"
-
+#include <algorithm>
 #include <list>
 #include <vector>
 
-std::vector< char > foo(std::list< Human >& people)
-{
-    // Twoja implementacja tutaj
-    return {};
+std::vector<char> foo(std::list<Human>& people) {
+    std::vector<char> retval(people.size());
+
+    // Inkrementowanie wieku każdej osoby w liście
+    std::for_each(people.begin(), people.end(), [](Human& h) { h.birthday(); });
+
+    // Tworzenie wektora wynikowego w odwrotnej kolejności
+    std::transform(people.rbegin(), people.rend(), retval.begin(), [](Human& h) {
+        // Jeżeli osoba jest 'monsterem' (nie lubi psa ani kota), to 'n'
+        return h.isMonster() ? 'n' : 'y';
+    });
+
+    return retval;
 }
+
+
